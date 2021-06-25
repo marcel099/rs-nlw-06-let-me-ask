96a2b38753477642c260530d5e3e17d2bab7e4a7
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-25 04:15:22 -0300

    Finalização da aula 02

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router'
+
 // webpack (snowpak, vite, ...) foi o estopim pra coisas como importação de imagem
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -5,9 +7,22 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
 
+import { useAuth } from '../hooks/useAuth'
+
 import '../styles/auth.scss'
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
+  
+  async function handleCreateRoom() {
+    if (!user) {
+      signInWithGoogle()
+    }
+
+    history.push('/rooms/new')
+  }
+  
   return (
     <div id="page-auth">
       <aside>
@@ -24,7 +39,10 @@ export function Home() {
             src={logoImg}
             alt="let me ask"
           />
-          <button className="create-room">
+          <button
+            className="create-room"
+            onClick={handleCreateRoom}
+          >
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
