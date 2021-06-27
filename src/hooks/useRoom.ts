bf12fedc42f6dff97d5d2838b985d09a2eeb68cc
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-26 23:33:40 -0300

    Features like, excl. de perg. e exc. de sala

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { database } from "../services/firebase"
+import { useAuth } from "./useAuth"
 
 type FirebaseQuestions = Record<string, {
   author: {
@@ -9,6 +10,9 @@ type FirebaseQuestions = Record<string, {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>;  
 }>
 
 type QuestionType = {
@@ -20,9 +24,14 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 }
 
 export function useRoom(roomId: string) {
+  const {
+    user,
+  } = useAuth()
   const [questions, setQuestions] = useState<QuestionType[]>([])
   const [title, setTitle] = useState('')
 
@@ -40,13 +49,19 @@ export function useRoom(roomId: string) {
           author: value.author,
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
         }
       })
 
       setTitle(databaseRoom.title)
       setQuestions(parsedQuestions)
     })
-  }, [roomId])
+
+    return () => {
+      roomRef.off('value')
+    }
+  }, [roomId, user?.id])
 
   return { questions, title }
 }
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,24 +1,26 @@
-import { FormEvent, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
 
 import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
-
-import { database } from '../services/firebase'
 
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
 
 import '../styles/room.scss'
 import { Question } from '../components/Question'
+import { database } from '../services/firebase'
 
 type RoomParams = {
   id: string;
 }
 
 export function AdminRoom() {
+  const history = useHistory()
+
   const { user } = useAuth();
 
   const params = useParams<RoomParams>();
@@ -31,6 +33,20 @@ export function AdminRoom() {
     questions,
   } = useRoom(roomId)
 
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      closedAt: new Date(),
+    })
+
+    history.push('/')
+  }
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+    }
+  }
+
 
   return (
     <div id="page-room">
@@ -39,7 +55,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Let me ask" />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>
         </div>
       </header>
@@ -59,7 +75,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             )
           })}
         </div>
